285b1e23250cc3a9a94c5947ffb99a792213e013
Mark Nguyen <markworks12@gmail.com>
2023-09-17 23:56:01 -0500

    navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,10 +10,10 @@ export default function NavBar() {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="nav-bar-collapse">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#experience">Experience</Nav.Link>
-              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="#project">Projects</Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
