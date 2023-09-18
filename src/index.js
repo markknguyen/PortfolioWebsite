3ee8ae7aa634b3e7dfc675b79b195e4d0b3b788d
Mark Nguyen <markworks12@gmail.com>
2023-09-17 23:43:39 -0500

    directed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/PortfolioWebsite">
       <App />
     </Router>
   </React.StrictMode>
